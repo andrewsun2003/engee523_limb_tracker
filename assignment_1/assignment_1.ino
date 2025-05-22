8ce9bf4311b93d0ecec330ed92cd9930d35b94cf
Andrew Sun <andrewsun2003@gmail.com>
2025-05-22 17:16:21 +1200

    Update assignment_1.ino

--- assignment_1/assignment_1.ino
+++ assignment_1/assignment_1.ino
@@ -80,7 +80,7 @@ void setup(void) {
 void loop() {
   if (imu.getSensorEvent(&sensorValue)) {
     switch (sensorValue.sensorId) {
-      case SH2_ACCELEROMETER: 
+      case SH2_ACCELEROMETER: {
         // unsigned long currentTime = micros();
         // float dt = (currentTime - lastTime) / 1000000.0;
         // lastTime = currentTime;
@@ -89,49 +89,51 @@ void loop() {
         rd.accel_z = sensorValue.un.accelerometer.z;
 
         break;
-      // case SH2_GYROSCOPE_CALIBRATED: 
-      //   unsigned long currentTime = micros();
-      //   float dt = (currentTime - lastTime) / 1000000.0;
-      //   lastTime = currentTime;
+      }
+      case SH2_GYROSCOPE_CALIBRATED: {
+        unsigned long currentTime = micros();
+        float dt = (currentTime - lastTime) / 1000000.0;
+        lastTime = currentTime;
 
-      //   rd.gyro_x = sensorValue.un.gyroscope.x;
-      //   rd.gyro_y = sensorValue.un.gyroscope.y;
-      //   rd.gyro_z = sensorValue.un.gyroscope.z;
+        rd.gyro_x = sensorValue.un.gyroscope.x;
+        rd.gyro_y = sensorValue.un.gyroscope.y;
+        rd.gyro_z = sensorValue.un.gyroscope.z;
 
-      //   rd.gyro_roll += rd.gyro_x * dt * RAD_TO_DEG;
-      //   rd.gyro_pitch += rd.gyro_y * dt * RAD_TO_DEG;
-      //   rd.gyro_yaw += rd.gyro_z * dt * RAD_TO_DEG;
+        rd.gyro_roll += rd.gyro_x * dt * RAD_TO_DEG;
+        rd.gyro_pitch += rd.gyro_y * dt * RAD_TO_DEG;
+        rd.gyro_yaw += rd.gyro_z * dt * RAD_TO_DEG;
 
-      //   Serial.print("GYRO (RPY): "); 
-      //   Serial.print(rd.gyro_roll);
-      //   Serial.print("\t");
-      //   Serial.print(rd.gyro_pitch);
-      //   Serial.print("\t");
-      //   Serial.println(rd.gyro_yaw);
+        Serial.print("GYRO (RPY): "); 
+        Serial.print(rd.gyro_roll);
+        Serial.print("\t");
+        Serial.print(rd.gyro_pitch);
+        Serial.print("\t");
+        Serial.println(rd.gyro_yaw);
 
-      //   break;
-      case SH2_MAGNETIC_FIELD_CALIBRATED:
+        break;
+      }
+      case SH2_MAGNETIC_FIELD_CALIBRATED: {
         rd.mag_x = sensorValue.un.magneticField.x;
         rd.mag_y = sensorValue.un.magneticField.y;
         rd.mag_z = sensorValue.un.magneticField.z;
 
-        // Serial.print("MAG: ");
-        // Serial.print(rd.mag_x);
-        // Serial.print("\t");
-        // Serial.print(rd.mag_y);
-        // Serial.print("\t");
-        // Serial.println(rd.mag_z);
-
         break;
       }
-      rd.accel_roll = atan2f(rd.accel_y, rd.accel_z) * RAD_TO_DEG;
-      rd.accel_pitch = atan2f(-rd.accel_x, sqrtf(rd.accel_y * rd.accel_y + rd.accel_z * rd.accel_z)) * RAD_TO_DEG;
-      rd.mag_yaw = atan2f(rd.mag_y, rd.mag_x) * RAD_TO_DEG;
-      // Serial.print("ACCEL (RP): "); 
-      Serial.print(millis()/1000); Serial.print("\t");
-      Serial.print(rd.accel_pitch); Serial.print("\t");
-      Serial.print(rd.accel_roll); Serial.print("\t");
-      Serial.println(rd.mag_yaw);
     }
+
+    rd.accel_roll = atan2f(rd.accel_y, rd.accel_z) * RAD_TO_DEG;
+    rd.accel_pitch = atan2f(-rd.accel_x, sqrtf(rd.accel_y * rd.accel_y + rd.accel_z * rd.accel_z)) * RAD_TO_DEG;
+    rd.mag_yaw = atan2f(rd.mag_y, rd.mag_x) * RAD_TO_DEG;
+
+    // Serial.print(millis()/1000); Serial.print("\t");
+    // Serial.print(rd.accel_pitch); Serial.print("\t");
+    // Serial.print(rd.accel_roll); Serial.print("\t");
+    // Serial.print(rd.mag_yaw); Serial.print("\t");
+
+    // Serial.print(rd.gyro_pitch); Serial.print("\t");
+    // Serial.print(rd.gyro_roll); Serial.print("\t");
+    // Serial.println(rd.gyro_yaw); 
+    }
+    
     delay(10);
 }
