a22a58bc7775a996f5c7323b2fc4331f59de24d7
Andrew Sun <andrewsun2003@gmail.com>
2025-05-24 19:26:36 +1200

    Update sensor_fusion.ino

--- sensor_fusion/sensor_fusion.ino
+++ sensor_fusion/sensor_fusion.ino
@@ -21,16 +21,11 @@ struct raw_data {   // Raw data
   float mag_x, mag_y, mag_z;
 } rd;
 
-struct {    // Raw angle data
+struct raw_angle_data {    // Raw angle data
   float accel_roll, accel_pitch, mag_yaw;
-  float gyro_roll = 0, gyro_pitch = 0, gyro_yaw = 0;
 } rad;
 
-Adafruit_BNO08x imu(IMU1_RESET);
-sh2_SensorValue_t sensorValue;
-
-long reportInterval = 100000;
-unsigned long lastTime;
+float x[3] = {0, 0, 0};   // State estimate
 
  float P[3][3] = {  // Covariance matrix (3x3)
   {0.1, 0, 0},
@@ -38,16 +33,35 @@ unsigned long lastTime;
   {0, 0, 0.1}
  };
 
+float  A[3][3] = {
+    {1, 0, 0},
+    {0, 1, 0},
+    {0, 0, 1}
+};  
+
 const float Q[3][3] = {   // Process noise covariance
   {0.001, 0, 0},
   {0, 0.001, 0},
   {0, 0, 0.001}
 };
 
-const float R[2][2] = {
-  {0.03, 0},
-  {0, 0.03}
+const float R[3][3] = {
+  {0.03, 0, 0},
+  {0, 0.03, 0},
+  {0, 0, 0.05}
 };
+
+const float I[3][3] = {
+  {1, 0, 0},
+  {0, 1, 0},
+  {0, 0, 1}
+};
+
+Adafruit_BNO08x imu(IMU1_RESET);
+sh2_SensorValue_t sensorValue;
+
+long reportInterval = 100000;
+unsigned long lastTime;
 
 
 void setReports() {
@@ -100,15 +114,93 @@ float wrap_angle(float angle) {
 }
 
 
-void ekf_update(rad angle_data) {
-  float  A[3][3] = {
-    {1, 0, 0},
-    {0, 1, 0},
-    {0, 0, 1}
-  }  
+void multiply3x3(const float A[3][3], const float B[3][3], float result[3][3]) {
+    // Initialize result matrix to zero
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            result[i][j] = 0.0f;
 
-  P
+    // Perform matrix multiplication
+    for (int i = 0; i < 3; i++) {         // row of A
+        for (int j = 0; j < 3; j++) {     // column of B
+            for (int k = 0; k < 3; k++) { // shared dimension
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
 
+
+void invert3x3(const float in[3][3], float out[3][3]) {
+  float det =
+    in[0][0] * (in[1][1] * in[2][2] - in[1][2] * in[2][1]) -
+    in[0][1] * (in[1][0] * in[2][2] - in[1][2] * in[2][0]) +
+    in[0][2] * (in[1][0] * in[2][1] - in[1][1] * in[2][0]);
+
+  float invDet = 1.0f / det;
+
+  out[0][0] =  (in[1][1]*in[2][2] - in[1][2]*in[2][1]) * invDet;
+  out[0][1] = -(in[0][1]*in[2][2] - in[0][2]*in[2][1]) * invDet;
+  out[0][2] =  (in[0][1]*in[1][2] - in[0][2]*in[1][1]) * invDet;
+
+  out[1][0] = -(in[1][0]*in[2][2] - in[1][2]*in[2][0]) * invDet;
+  out[1][1] =  (in[0][0]*in[2][2] - in[0][2]*in[2][0]) * invDet;
+  out[1][2] = -(in[0][0]*in[1][2] - in[0][2]*in[1][0]) * invDet;
+
+  out[2][0] =  (in[1][0]*in[2][1] - in[1][1]*in[2][0]) * invDet;
+  out[2][1] = -(in[0][0]*in[2][1] - in[0][1]*in[2][0]) * invDet;
+  out[2][2] =  (in[0][0]*in[1][1] - in[0][1]*in[1][0]) * invDet;
+}
+
+
+void ekf_update(struct raw_angle_data *rad, float x[3], float P[3][3], const float A[3][3], const float Q[3][3], const float R[3][3], const float I[3][3]) { 
+  float z[3] = {rad->accel_roll, rad->accel_pitch, rad->mag_yaw};
+
+  // State covariance: P = A*P*A^T + Q (A = identity matrix)
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      P[i][j] += Q[i][j];
+
+  // Measurement residual: y = z - Hx
+  float y[3];
+  for (int i = 0; i < 3; i++) {
+    y[i] = z[i] - x[i];
+    y[i] = wrap_angle(y[i]);
+  }
+
+  // Innovation covariance: S = H*P*H^T + R
+  float S[3][3];
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      S[i][j] = P[i][j] + R[i][j];
+      // Serial.print(S[0][0]);
+  
+  // Kalman gain: K = P*H^T*S^-1
+  float S_inv[3][3];
+  invert3x3(S, S_inv);
+  float K[3][3];
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      K[i][j] = P[i][j] * S_inv[i][j];
+
+  // Update state: x = x + K*y
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      x[i] += K[i][j] * y[j];
+    }
+    x[i] = wrap_angle(x[i]);
+  }
+
+  // Update state covariance: P = (I - KH)*P
+  float I_minus_k[3][3];
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+        I_minus_k[i][j] = I[i][j] - K[i][j];
+      }
+  }
+
+  multiply3x3(P, I_minus_k, P);
 
 }
 
@@ -146,13 +238,13 @@ void loop() {
         // rad.gyro_pitch += rad.gyro_pitch_rate * dt * RAD_TO_DEG;
         // rad.gyro_yaw += rad.gyro_yaw_rate * dt * RAD_TO_DEG;
 
-        rad.gyro_roll += rd.gyro_x * dt * RAD_TO_DEG;
-        rad.gyro_pitch += rd.gyro_y * dt * RAD_TO_DEG;
-        rad.gyro_yaw += rd.gyro_z * dt * RAD_TO_DEG;
+        x[0] += rd.gyro_x * dt * RAD_TO_DEG;
+        x[1] += rd.gyro_y * dt * RAD_TO_DEG;
+        x[2] += rd.gyro_z * dt * RAD_TO_DEG;
 
-        rad.gyro_roll = wrap_angle(rad.gyro_roll);
-        rad.gyro_pitch = wrap_angle(rad.gyro_pitch);
-        rad.gyro_yaw = wrap_angle(rad.gyro_yaw);
+        x[0] = wrap_angle(x[0]);
+        x[1] = wrap_angle(x[1]);
+        x[2] = wrap_angle(x[2]);
 
         break;
       }
@@ -167,22 +259,20 @@ void loop() {
       }
     }
 
-    // RAW DATA
-    
     Serial.print(millis()/1000); Serial.print("\t");
-    Serial.print(rd.accel_roll); Serial.print("\t");
 
-    Serial.print(rd.accel_pitch); Serial.print("\t");
-    Serial.println(rd.mag_yaw);
+    // RAW DATA
+    // Serial.print(rad.accel_roll); Serial.print("\t");
+    // Serial.print(rad.accel_pitch); Serial.print("\t");
+    // Serial.print(rad.mag_yaw); Serial.print("\t");
 
     // SENSOR FUSION DATA (APPLIED EKF)
-    ekf_update(rad);
+    ekf_update(&rad, x, P, A, Q, R, I);
 
-    // Serial.print(rad.gyro_roll); Serial.print("\t");
-    // Serial.print(rad.gyro_pitch); Serial.print("\t");
-    // Serial.println(rad.gyro_yaw); 
+    Serial.print(x[0]); Serial.print("\t");
+    Serial.print(x[1]); Serial.print("\t");
+    Serial.println(x[2]); 
     }
     
     delay(10);
 }
-
