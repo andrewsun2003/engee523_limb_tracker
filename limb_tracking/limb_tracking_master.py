aa93f6a245de8ca25625e198cc617d0db5df1e66
Andrew Sun <andrewsun2003@gmail.com>
2025-05-26 00:10:02 +1200

    Add files via upload

---
+++ limb_tracking/limb_tracking_master.py
@@ -0,0 +1,8 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("0.0.0.0", 1234))
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print(f"{addr}: {data.decode()}")
